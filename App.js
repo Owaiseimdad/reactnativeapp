2993e86ccc389ea2b008d53ce47a532f42bf30c2
Owaise Imdad <owaiseimdad@gmail.com>
2024-02-25 20:00:30 +0530

    React Native app with two inputs and one button.

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import react from "react";
+import { useState } from "react/cjs/react.development";
 export default function App() {
+  const [name, setName] = useState("John");
+  const [age, setAge] = useState(0);
+  const [textName, setTextName] = useState({});
+  const [textAge, setTextAge] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.textheader}>
+          Hello {name} !!! ({age} years old)
+        </Text>
+      </View>
+      <View>
+        <TextInput
+          style={styles.textinput}
+          placeholder="e.g. John"
+          onChangeText={(e) => {
+            setTextName(e);
+          }}
+        />
+      </View>
+      <View>
+        <TextInput
+          keyboardType="numeric"
+          style={styles.textinput}
+          placeholder="e.g. 65"
+          onChangeText={(e) => {
+            setTextAge(e);
+          }}
+        />
+      </View>
+      <View style={styles.btn}>
+        <Button
+          title="update"
+          onPress={() => {
+            setName(textName);
+            setAge(textAge);
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +50,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  header: {
+    backgroundColor: "pink",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+    alignItems: "top ",
+  },
+  textheader: {
+    color: "black",
+    fontWeight: "bold",
+  },
+  textinput: {
+    borderColor: "#7777",
+    borderRadius: 5,
+    borderWidth: 1,
+    padding: 10,
+    margin: 5,
+    width: 200,
+    backgroundColor: "white",
+  },
+  btn: { borderWidth: 1, padding: 10 },
 });
