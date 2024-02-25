53348a5fddbbe9a09145cce1b8b6ca8f73abdb62
Owaise Imdad <owaiseimdad@gmail.com>
2024-02-25 20:24:07 +0530

    Added data.js which returns values and then made a page scrollable.

--- App.js
+++ App.js
@@ -1,48 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import react from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useState } from "react/cjs/react.development";
+import { data } from "./data";
+
 export default function App() {
   const [name, setName] = useState("John");
   const [age, setAge] = useState(0);
   const [textName, setTextName] = useState({});
   const [textAge, setTextAge] = useState(0);
 
+  const printTouchedvalue = (e) => {
+    console.log(e);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.textheader}>
-          Hello {name} !!! ({age} years old)
-        </Text>
-      </View>
-      <View>
-        <TextInput
-          style={styles.textinput}
-          placeholder="e.g. John"
-          onChangeText={(e) => {
-            setTextName(e);
-          }}
-        />
-      </View>
-      <View>
-        <TextInput
-          keyboardType="numeric"
-          style={styles.textinput}
-          placeholder="e.g. 65"
-          onChangeText={(e) => {
-            setTextAge(e);
-          }}
-        />
-      </View>
-      <View style={styles.btn}>
-        <Button
-          title="update"
-          onPress={() => {
-            setName(textName);
-            setAge(textAge);
-          }}
-        />
-      </View>
+      <ScrollView>
+        {data.map((item) => (
+          <TouchableOpacity
+            key={item.id}
+            style={styles.user}
+            onPress={() => {
+              printTouchedvalue(item.email);
+            }}
+          >
+            <Text>First Name: {item.firstName}</Text>
+            <Text>Last Name: {item.lastName}</Text>
+            <Text>Location: {item.address.city}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -50,29 +45,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    backgroundColor: "pink",
-    padding: 10,
-    margin: 5,
-    borderRadius: 5,
-    alignItems: "top ",
-  },
-  textheader: {
-    color: "black",
-    fontWeight: "bold",
-  },
-  textinput: {
-    borderColor: "#7777",
-    borderRadius: 5,
-    borderWidth: 1,
-    padding: 10,
-    margin: 5,
-    width: 200,
     backgroundColor: "white",
+    marginTop: 30,
   },
-  btn: { borderWidth: 1, padding: 10 },
+  user: {
+    backgroundColor: "grey",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+  },
 });
